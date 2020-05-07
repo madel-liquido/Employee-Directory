211308d84be25109d62d716d737dc39b9ff48f10
Madel Liquido <madel.liquido@gmail.com>
2020-05-07 19:37:09 -0400

    Working on mutating state

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,15 +1,9 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
-
 export default {
-  getRandomDog: function() {
-    return axios.get("https://dog.ceo/api/breeds/image/random");
-  },
-  getDogsOfBreed: function(breed) {
-    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-  },
-  getBaseBreedsList: function() {
-    return axios.get("https://dog.ceo/api/breeds/list");
+  // Gets all users
+  getUsers: function() {
+
+    return axios.get("https://randomuser.me/api/?results=200&nat=us");
   }
 };
